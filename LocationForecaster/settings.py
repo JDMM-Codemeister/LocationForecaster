b48b297fa42d70ef919c875e9f9929981f04b49b
JDMM <jack.dmm91@gmail.com>
2025-08-12 21:57:12 -0600

    Generate new django key and .gitignore

--- LocationForecaster/settings.py
+++ LocationForecaster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from schluesselwoerter import django_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qzxm7ns5s6@t^tn2u10)d0dl*c7c(47&kuedilarg82(61h+2v'
+SECRET_KEY = django_key
 
+#TODO
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
