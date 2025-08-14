b1f16100f4f76bf6bda0c3f8734117535d06b886
JDMM <jack.dmm91@gmail.com>
2025-08-13 18:58:36 -0600

    Added Leaflet map

--- LocFor_app/urls.py
+++ LocFor_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("todos/", views.todos, name="Todos"),
-    path("about/", views.about, name="bout"),
+    path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
--- LocFor_app/views.py
+++ LocFor_app/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, HttpResponse
 from .models import TodoItem
 # Create your views here.
 def home(request):
-    location = request.GET.get('location', '')  # Get input, or empty string if none
+    location = request.GET.get("location")  # Get search from form
     forecast_result = None
 
     if location:
-        # Find coordinates on map and load forecasts
-        forecast_result = f"Forecast for {location} is sunny!"  # TODO REPLACE WITH TRIGGER MAP UPDATE
+        # TODO call forecast API function
+        forecast_result = f"Example forecast for {location}"
 
     return render(request, 'home.html', {
-        'location': location,
-        'forecast_result': forecast_result,
+        "location": location,
+        "forecast_result": forecast_result
     })
 
 def contact(request):
